9d5c629cf36269872dc363ce507037e7440d67de
olegarionet0 <163175169+olegarionet0@users.noreply.github.com>
2025-04-29 23:36:18 -0300

    correção no test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:meu_app_flutter_clone/main.dart';
+import 'package:meu_app_flutter_clone/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
