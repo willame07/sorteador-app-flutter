9ce29af262b54de84d164034471df867927f60a5
willame07 <joaowillame21@gmail.com>
2025-05-07 16:18:08 -0300

    modificaçoes referentes ao poscionamento
    dia 06-05-25

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:meu_app_flutter_clone/services/sorteador_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var numeroSorteado = 0;
+  var qtdSorteios = 0;
+  var ultimosorteio = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,12 +21,28 @@ class _HomePageState extends State<HomePage> {
         title: Text("Meu Primeiro App Flutter"),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: Center(child: Text(numeroSorteado.toString())),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Tivemos até agora $qtdSorteios sorteios!", 
+            style:GoogleFonts.tajawal(fontSize: 48)),
+          Text(
+            numeroSorteado.toString(), 
+            style:GoogleFonts.tajawal(fontSize: 100)),
+          Text(
+            "O ultimo número sorteado foi: $ultimosorteio", 
+            style:GoogleFonts.tajawal(fontSize: 48)),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.monetization_on_outlined),
         onPressed: () {
           setState(() {
+            qtdSorteios++;
             numeroSorteado = SorteadorService.sortearNumero();
+            ultimosorteio = numeroSorteado;
           });
           debugPrint(numeroSorteado.toString());
         },
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'Pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: false, primarySwatch: Colors.pink),
+      theme: ThemeData(useMaterial3: false, primarySwatch: Colors.deepPurple,
+      textTheme: GoogleFonts.abelTextTheme()),
       home: HomePage(),
     );
   }
